0367e8b9c2aa1d245e83d48b8068ca85ec7b81ac
Thomas J. Webb <thomas@osakared.com>
2023-09-27 14:36:35 -0700

    Good enough to show people

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,33 @@
+import js.Browser;
+import js.html.InputElement;
+
+typedef Pair = {
+    label:String,
+    value:String
+}
 
 class Main
 {
     static function main() {
-        var x = new Number(255);
-        trace(x.baseFour(Nucleobase));
+        var input:InputElement = cast Browser.document.getElementById("numberEntry");
+        var output = Browser.document.getElementById("numberRepresentation");
+        var updateDisplay = () -> {
+            var value:Null<Int> = Std.parseInt(input.value);
+            var innerHTML = if (value == null) '';
+            else {
+                var number = new Number(value);
+                var outputs = new Array<Pair>();
+                outputs.push({label: "Arabic", value: number.baseFour(Arabic)});
+                outputs.push({label: "Suits", value: number.baseFour(Suits)});
+                outputs.push({label: "Nucleobases", value: number.baseFour(Nucleobase)});
+                outputs.push({label: "Ventureño Chumash", value: number.baseFour(Ventureno)});
+                [for (pair in outputs) '<b>${pair.label}:</b> ${pair.value}'].join("<br />\n");
+            }
+            output.innerHTML = innerHTML;
+        }
+        // Browser.window.onload = updateDisplay;
+        input.oninput = updateDisplay;
+        // var x = new Number(255);
+        // trace(x.baseFour(Nucleobase));
     }
 }
--- src/Number.hx
+++ src/Number.hx
@@ -3,6 +3,7 @@ enum Representation {
     Arabic;
     Suits;
     Nucleobase;
+    Ventureno;
 }
 
 class Number
@@ -11,6 +12,45 @@ class Number
 
     public function new(number:Int) {
         this.number = number;
+    }
+
+    private static function venturenoName(number:Int):String {
+        return switch number {
+            case 1:  "pake'et";
+            case 2:  "'iškom̓";
+            case 3:  "masǝx";
+            case 4:  "tskumu";
+            case 5:  "yǝtipake'es";
+            case 6:  "yǝti'iškom̓";
+            case 7:  "yǝtimasǝx";
+            case 8:  "malawa";
+            case 9:  "tspa";
+            case 10: "ka'aškom";
+            case 11: "tǝlu";
+            case 12: "masǝx tskumu";
+            case 13: "masǝx tskumu kampake'et";
+            case 14: "'iškom̓ laliet";
+            case 15: "pake'et siwe (tšikipš)";
+            case 16: "tšikipš";
+            case 17: "tšikipš kampake'et";
+            case 18: "'iškom̓ siwe tskumu'uy";
+            case 19: "pake'et siwe tskumu'uy";
+            case 20: "tskumu'uy";
+            case 21: "tskumu'uy kampake'et";
+            case 22: "'iškom̓ siwe itsmaxmasǝx";
+            case 23: "pake'et siwe itsmaxmasǝx";
+            case 24: "itsmaxmasǝx";
+            case 25: "itsmaxmasǝx kampake'et";
+            case 26: "'iškom̓ siwe yitimasǝx";
+            case 27: "pake'et siwe yitimasǝx";
+            case 28: "yitimasǝx";
+            case 29: "yitimasǝx kampake'et";
+            case 30: "'iškom̓ siwe 'iškom̓ tšikipš";
+            case 31: "pake'et siwe 'iškom̓ tšikipš";
+            case 32: "'iškom̓ tšikipš"; // highest recorded number, in any Chumashan language
+            case 33: "'iškom̓ tšikipš kampake'et"; // my reasonable speculation
+            default: '(unknown)'; // we don't know because every four numbers is a unique name
+        }
     }
 
     private static function representationOfDigit(baseFourNumber:Int, representation:Representation):String {
@@ -33,11 +73,19 @@ class Number
                     case 3: 'G';
                     default: '';
                 }
+            case Ventureno:
+                ''; // not implemented this way
         }
     }
 
     private static function baseFourForNumber(number:Int, representation:Representation):String {
-        return if (number < 4)
+        if (representation == Ventureno) return venturenoName(number);
+        var sign = '';
+        if (number < 0) {
+            number *= -1;
+            sign = '-';
+        }
+        return sign + if (number < 4)
             representationOfDigit(number, representation);
         else {
             var result = Std.int(number / 4);
